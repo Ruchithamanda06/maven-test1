bd103949a1fb8fcf43b06381e7cc1c5f611f688c
Ruchitha <ruchithamanda06@gmail.com>
2023-10-20 00:14:53 +0530

    FEATURE?RUCHITHAM0001- Wrote the code for adding two numbers

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,16 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    
+            public static int ruchitha(int num1, int num2) {
+                return num1 + num2;
+            }
+        
+            public static void main(String[] args) {
+                int result = ruchitha(5, 7);
+                System.out.println("The sum is: " + result);
+            }
+        
+        
+    }
+
